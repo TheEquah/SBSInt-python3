1a1052ed9072cc61f84e1088fe6f3edd41c07087
TheEquah <theequah@gmail.com>
2020-03-22 17:21:39 +0100

    Single byte processing implementation.

--- src/equah/sbsint/__init__.py
+++ src/equah/sbsint/__init__.py
@@ -1,5 +1,7 @@
 # Author (Created): Roger "Equah" Hürzeler
+# Author (Modified): Roger "Equah" Hürzeler
 # Date (Created): 12019.12.25 HE
+# Date (Modified): 12020.03.22 HE
 # License: apache-2.0
 
 
@@ -23,6 +25,23 @@ def list_to_int(bl) :
     
     return (sbsint, size)
 
+# [>] Single Bytes To Integer
+# [i] Uses a given function to read single bytes of an SBSInt and converts it to an integer.
+# [P] {function () > int} fn_r => Function returning the next byte.
+# [R] {(int, int)} => Tuple of SBSInt value.
+def sbytes_to_int(fn_r) :
+	
+	size = 1
+	sbsint = 0
+	next_byte = fn_r()
+	while next_byte == 0xFF :
+		sbsint += 0xFF
+		size += 1
+		pass
+	sbsint += next_byte
+	
+	return (sbsint, size)
+
 # [>] Integer To Bytearray
 # [i] Converts an integer to SBSInt bytearray.
 # [P] {int} i => Integer to convert to SBSInt.
@@ -41,3 +60,22 @@ def int_to_bytearray(i, buff) :
     buff.append(i)
     
     return size
+
+# [>] Integer To Single Bytes
+# [i] Converts given integer to SBSInt andwrites it with given function in single bytes.
+# [P] {int} i => Integer to convert to SBSInt bytes.
+# [P] {function (int)} => Function to write a single byte.
+# [R] {int} => Amount of written bytes.
+def int_to_sbytes(i, fn_w) :
+	
+	size = 1
+	
+	while i >= 0xFF :
+		fn_w(0xFF)
+		i -= 0xFF
+		size += 1
+		pass
+	
+	fn_w(i)
+	
+	return size
